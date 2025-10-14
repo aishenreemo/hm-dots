d621226bb296416c786021dafa562f27c4adf6df
Aishen Reemo <aishenreemo@tilde.club>
2025-10-14 19:48:10 +0800

    fix(fish): misconfigured GPG_TTY

--- fish.nix
+++ fish.nix
@@ -7,7 +7,7 @@
     alias grep='grep --color=auto'
     alias alacritty="nixGL alacritty"
     set -Ux EDITOR nvim
-    set -Ux GPG_TTY (tty)
+    set -gx GPG_TTY (tty)
     set -gx PATH /nix/var/nix/profiles/default/bin:$HOME/.local/bin:$HOME/.nix-profile/bin:/usr/bin
     set -gx XDG_CONFIG_HOME $HOME/.config
     set -x XDG_DATA_DIRS "$HOME/.local/share:$HOME/.nix-profile/share:$XDG_DATA_HOME/share:/usr/local/share:/usr/share"
