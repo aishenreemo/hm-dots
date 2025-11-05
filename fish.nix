f76dff2ef3e3ad2356777ac0477f5ae1f1e331e8
Aishen Reemo <aishenreemo@tilde.club>
2025-11-05 21:56:11 +0800

    feat(fish): added flatpak to $XDG_DATA_DIRS
    - i did this to add roblox desktop icon on my wofi lol

--- fish.nix
+++ fish.nix
@@ -10,7 +10,7 @@
     set -gx GPG_TTY (tty)
     set -gx PATH /nix/var/nix/profiles/default/bin:$HOME/.local/bin:$HOME/.nix-profile/bin:/usr/bin
     set -gx XDG_CONFIG_HOME $HOME/.config
-    set -x XDG_DATA_DIRS "$HOME/.local/share:$HOME/.nix-profile/share:$XDG_DATA_HOME/share:/usr/local/share:/usr/share"
+    set -x XDG_DATA_DIRS "$HOME/.local/share:$HOME/.nix-profile/share:$XDG_DATA_HOME/share:/usr/local/share:/usr/share:$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share"
     set fish_greeting
     if test (tty) = "/dev/tty1"
       if not set -q WAYLAND_DISPLAY
